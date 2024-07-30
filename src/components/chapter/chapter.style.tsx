de47efeb2b06ddd74da80d7d37e1e0b2713d17e6
Abanoub Ragaay <abanoub.ragaay@CLPDC.LOCAL>
2024-07-30 16:42:18 +0300

    fixing popup events issue

--- src/components/chapter/chapter.style.tsx
+++ src/components/chapter/chapter.style.tsx
@@ -4,11 +4,9 @@ export const ChapterWrapper = styled.div`
   display: block;
   width: 100%;
   height: 100vh;
-  background-color: #444444da;
   position: absolute;
-  top: 50%;
-  right: 50%;
-  transform: translate(50%, -50%);
+  top: 0;
+  right: 0;
   color: wheat;
   overflow: hidden;
   display: flex;
